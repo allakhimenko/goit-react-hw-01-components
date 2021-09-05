ff2730f036e892258cc9d9a57d6fd4f681a2f39c
allakhimenko <all.khimenko24@gmail.com>
2021-09-05 18:53:21 +0300

    Update FriendList.js

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,3 +1,9 @@
+import PropTypes from 'prop-types';
+
 export default function FriendList({ children }) {
   return <ul class="friend-list">{children}</ul>;
 }
+
+FriendList.PropType = {
+  children: PropTypes.node,
+};
