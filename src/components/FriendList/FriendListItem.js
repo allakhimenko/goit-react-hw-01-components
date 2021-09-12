e5b37be94a398ef9e864be51e31247e126ff67ce
allakhimenko <all.khimenko24@gmail.com>
2021-09-12 12:51:12 +0300

    Update FriendListItem.js

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -4,7 +4,7 @@ import s from './friendList.module.css';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={s.item}>
-      <span className={s[`${isOnline}`]}></span>
+      <span className={s[`${isOnline}`]} />
       <img className={s.avatar} src={avatar} alt={name} />
       <p className={s.name}>{name}</p>
     </li>
